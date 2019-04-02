f1127b821e6c0385cef7598b90db59f9d08b6498
KREDIT\song_se <song_se@kredit.com.kh>
2019-04-02 09:31:26 +0700

    Labels and Devtools IllegalStateException after devtools reload

--- zkspringboot-demos/zkspringboot-minimal-war/src/main/java/org/zkoss/zkspringboot/demo/MinimalWarApplication.java
+++ zkspringboot-demos/zkspringboot-minimal-war/src/main/java/org/zkoss/zkspringboot/demo/MinimalWarApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 //@Controller
@@ -18,8 +19,8 @@ public class MinimalWarApplication extends SpringBootServletInitializer {
 		SpringApplication.run(MinimalWarApplication.class);
 	}
 
-//	@GetMapping("/")
-//	public String demo() {
-//		return "/minimal";
-//	}
+	@GetMapping("/")
+	public String demo() {
+		return "/minimal";
+	}
 }
